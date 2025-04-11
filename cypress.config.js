4cc7f417b236c1d7afc29911bb5de04f27b6f058
Kleber de Andrade <kleber.moura@grupobrisanet.com.br>
2025-04-11 17:18:09 -0300

    Reset e alteração de contas.

--- cypress.config.js
+++ cypress.config.js
@@ -2,8 +2,7 @@ module.exports = {
   viewportWidth: 1280,
   viewportHeight: 720,
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    baseUrl: "https://barrigarest.wcaquino.me",
+    setupNodeEvents(on, config) {},
   },
 };
--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -9,11 +9,19 @@ describe("Acesso ao site Barriga React", () => {
       token = tkn;
     });
   });
-  beforeEach("Access", () => {});
+  beforeEach("Access", () => {
+    cy.request({
+      url: "/reset",
+      method: "GET",
+      headers: {
+        Authorization: `JWT ${token}`,
+      },
+    });
+  });
 
   it("Criação de conta", () => {
     cy.request({
-      url: "https://barrigarest.wcaquino.me/contas",
+      url: "/contas",
       method: "POST",
       headers: {
         Authorization: `JWT ${token}`,
@@ -27,11 +35,48 @@ describe("Acesso ao site Barriga React", () => {
       expect(res.body).to.have.property("id");
       expect(res.body).to.have.property("nome", "Conta via rest");
     });
-    /*cy.request({
+  });
+
+  it("Alteração de contas", () => {
+    cy.request({
       method: "GET",
-      url: "https://barrigarest.wcaquino.me/reset",
+      url: "/contas",
       headers: {
-        Authorization: "@response",
-      },*/
+        Authorization: `JWT ${token}`,
+      },
+      qs: {
+        nome: "Conta para alterar",
+      },
+    }).then((res) => {
+      cy.request({
+        url: `https://barrigarest.wcaquino.me/contas/${res.body[0].id}`,
+        method: "PUT",
+        headers: {
+          Authorization: `JWT ${token}`,
+        },
+        body: {
+          nome: "conta alterada via rest",
+        },
+      }).as("response");
+      cy.get("@response").its("status").should("be.equal", 200);
+    });
+  });
+
+  it("Conta duplicada", () => {
+    cy.request({
+      url: "/contas",
+      method: "POST",
+      headers: {
+        Authorization: `JWT ${token}`,
+      },
+      body: {
+        nome: "Conta mesmo nome",
+      },
+      failOnStatusCode: false,
+    }).as("response");
+    cy.get("@response").then((res) => {
+      expect(res.status).to.be.equal(400);
+      expect(res.body.error).to.be.equal("Já existe uma conta com esse nome!");
+    });
   });
 });
